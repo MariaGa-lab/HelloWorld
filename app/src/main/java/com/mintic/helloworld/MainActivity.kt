3f4e49d93734a9df2d1fceddf667f7b7558e7f00
MariaGa <mariamorera240514@gmail.com>
2021-11-15 10:32:55 -0500

    Creacion primera activity

--- app/src/main/java/com/mintic/helloworld/MainActivity.kt
+++ app/src/main/java/com/mintic/helloworld/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mintic.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tituloTextView : TextView = findViewById(R.id.titulo_text_view)
+
+        var nombre = intent.extras?.getString("Nombre")
+        tituloTextView.text = nombre
+
+        nombre?.let { Log.d("nombre", it) }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
